a465ed3f00bd37b91df32962d7a3fc2d511e39a6
Anmol Appinventiv <anmol.agarwal@appinventiv.com>
2026-07-07 18:40:20 +0530

    publish article fixed

--- src/PublishArticle/publishArticle.controller.ts
+++ src/PublishArticle/publishArticle.controller.ts
@@ -1,110 +1,180 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { PublishArticleService } from './publishArticle.service';
-import { CreateArticleDTO, UpdateArticleDTO } from './dto/publishArticle.dto';
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
   UploadedFile,
   UseInterceptors,
+  BadRequestException,
+  ParseIntPipe,
 } from '@nestjs/common';
+
+import {
+  ApiBearerAuth,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import { PublishArticleService } from './publishArticle.service';
+import {
+  CreateArticleDTO,
+  UpdateArticleDTO,
+} from './dto/publishArticle.dto';
+
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 
+
 @ApiTags('PublishArticle')
 @Controller('publish-article')
 export class PublishArticleController {
-    constructor(private readonly service: PublishArticleService) { }
-    
+
+  constructor(
+    private readonly service: PublishArticleService,
+  ) { }
 
 
-@Post()
-@UseGuards(JwtAuthGuard)
-@ApiBearerAuth()
-@UseInterceptors(
-  FileInterceptor('image', {
-    storage: diskStorage({
-      destination:
-        'C:/Users/user/Desktop/Website/news-hub/public/Images',
-      filename: (req, file, cb) => {
-        const uniqueName =
-          Date.now() +
-          '-' +
-          Math.round(Math.random() * 1e9) +
-          extname(file.originalname);
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination:
+          'C:/Users/user/Desktop/Website/news-hub/public/Images',
 
-        cb(null, uniqueName);
-      },
+        filename: (req, file, cb) => {
+
+          const fileName =
+            Date.now() +
+            '-' +
+            Math.round(Math.random() * 1e9) +
+            extname(file.originalname);
+
+          cb(null, fileName);
+        },
+      }),
     }),
-  }),
-)
-Create(
-  @UploadedFile() file: any,
-  @Body() dto: CreateArticleDTO,
-  @Req() req: any,
-) {
-  dto.author_id = req.user.Id;
+  )
+  async Create(
+    @UploadedFile() file: any,
+    @Body() dto: CreateArticleDTO,
+    @Req() req: any,
+  ) {
 
-  if (file) {
-    dto.featured_image = `./Images/${file.filename}`;
+    dto.author_id = req.user.Id;
+
+    if (file) {
+      dto.featured_image =
+        `./Images/${file.filename}`;
+    }
+
+
+    if (!dto.title) {
+      throw new BadRequestException(
+        'publish-article.Title_Required'
+      );
+    }
+
+    if (!dto.category_id) {
+      throw new BadRequestException(
+        'publish-article.Category_Required'
+      );
+    }
+
+
+    return this.service.Create(dto);
   }
 
-  return this.service.Create(dto);
-}
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    GetAll(
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async GetAll(
+
     @Query('pageNumber') pageNumber: number = 1,
+
     @Query('pageSize') pageSize: number = 10,
-    @Query('status_id') status_id: number,
-    @Query('category_id') category_id: number,
+
+    @Query('status_id') status_id: number = 0,
+
+    @Query('category_id') category_id: number = 0,
+
     @Query('search') search: string = '',
-    @Req() req: Request){
 
-        const request = req as any;
-        const userId = request.user.Id
-        
-        if(!status_id) {
-            status_id = 0;
-        }
-        if(!category_id) {
-            category_id = 0;
-        }
-        if(!search) {
-            search = '';
-        }
-        return this.service.GetAll(pageNumber,pageSize,status_id,category_id,search,userId);   
-    }
+    @Req() req: any,
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    GetById(
-        @Param('id') id: number
-    ) {
-        return this.service.GetById(id);
-    }
+  ) {
 
-    @Put(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    Update(
-        @Param('id') id: number,
-        @Body() dto: UpdateArticleDTO
-    ) {
-        dto.id = id;
-        return this.service.Update(dto);
-    }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    Delete(
-        @Param('id') id: number
-    ){
-        return this.service.Delete(id);
-    }
+    const userId = req.user.Id;
+
+
+    return this.service.GetAll(
+      pageNumber,
+      pageSize,
+      status_id,
+      category_id,
+      search,
+      userId,
+    );
+
+  }
+
+
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async GetById(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+
+    return this.service.GetById(id);
+
+  }
+
+
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async Update(
+
+    @Param('id', ParseIntPipe) id: number,
+
+    @Body() dto: UpdateArticleDTO,
+
+  ) {
+
+    dto.id = id;
+
+    return this.service.Update(dto);
+
+  }
+
+
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async Delete(
+
+    @Param('id', ParseIntPipe) id: number,
+
+  ) {
+
+    return this.service.Delete(id);
+
+  }
 
 }
--- src/PublishArticle/publishArticle.service.ts
+++ src/PublishArticle/publishArticle.service.ts
@@ -1,45 +1,114 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { PublishArticleRepository } from './publishArticle.repository';
-import { CreateArticleDTO, UpdateArticleDTO } from './dto/publishArticle.dto';
+import {
+  CreateArticleDTO,
+  UpdateArticleDTO,
+} from './dto/publishArticle.dto';
 
 @Injectable()
 export class PublishArticleService {
 
-
-    constructor(
-        private readonly repo: PublishArticleRepository
-    ) { }
-
+  constructor(
+    private readonly repo: PublishArticleRepository,
+  ) {}
 
 
-    Create(dto: CreateArticleDTO) {
-        return this.repo.Create(dto);
+  async Create(dto: CreateArticleDTO) {
+
+    const response = await this.repo.Create(dto);
+
+    if (!response.isSuccess) {
+      throw new BadRequestException(
+        'publish-article.Create_Failed'
+      );
     }
 
+    response.message = 'publish-article.Created';
+
+    return response;
+  }
 
 
-    GetAll(pageNumber: number, pageSize: number, status_id: number, category_id: number, search: string | '', userId: number) {
-        return this.repo.GetAll(pageNumber, pageSize, status_id, category_id, search, userId);
+  async GetAll(
+    pageNumber: number,
+    pageSize: number,
+    status_id: number,
+    category_id: number,
+    search: string = '',
+    userId: number,
+  ) {
+
+    const response = await this.repo.GetAll(
+      pageNumber,
+      pageSize,
+      status_id,
+      category_id,
+      search,
+      userId,
+    );
+
+    if (!response.isSuccess) {
+      throw new BadRequestException(
+        'publish-article.List_Failed'
+      );
     }
 
+    response.message = 'publish-article.List_Success';
+
+    return response;
+  }
 
 
-    GetById(id: number) {
-        return this.repo.GetById(id);
+  async GetById(id: number) {
+
+    const response = await this.repo.GetById(id);
+
+    if (!response.isSuccess || !response.response) {
+      throw new NotFoundException(
+        'publish-article.Not_Found'
+      );
     }
 
+    response.message = 'publish-article.Found';
+
+    return response;
+  }
 
 
-    Update(dto: UpdateArticleDTO) {
-        return this.repo.Update(dto);
+  async Update(dto: UpdateArticleDTO) {
+
+    const response = await this.repo.Update(dto);
+
+    if (!response.isSuccess) {
+      throw new BadRequestException(
+        'publish-article.Update_Failed'
+      );
     }
 
+    response.message = 'publish-article.Updated';
+
+    return response;
+  }
 
 
-    Delete(id: number) {
-        return this.repo.Delete(id);
+  async Delete(id: number) {
+
+    const response = await this.repo.Delete(id);
+
+    if (!response.isSuccess) {
+      throw new BadRequestException(
+        'publish-article.Delete_Failed'
+      );
     }
 
+    response.message = 'publish-article.Deleted';
 
+    return response;
+  }
 
 }
